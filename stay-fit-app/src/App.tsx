56b94ea063ae077885e258dc132e200a47f226b2
Rahul Bandodkar <rahul.bandodkar@creativecapsule.com>
2023-03-12 23:52:18 +0530

    Added tailwind css

--- stay-fit-app/src/App.tsx
+++ stay-fit-app/src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label
+        htmlFor="about"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        About
+      </label>
+      <div className="mt-2">
+        <textarea
+          id="about"
+          name="about"
+          rows={3}
+          className="mt-1 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+          placeholder="you@example.com"
+        ></textarea>
+      </div>
+      <p className="mt-2 text-sm text-gray-500">
+        Brief description for your profile. URLs are hyperlinked.
+      </p>
     </div>
   );
 }
